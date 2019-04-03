e0681b8f95338998ba0fd5ae98fb6ec0dd870138
Tommy <twmay98@hotmail.com>
2019-04-03 13:41:10 -0700

    styling on pages, seperated into js and css files

---
+++ firebase_setup.js
@@ -0,0 +1,13 @@
+(function (){
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBoWW88WXnJm6ak-R7H8OabzgNWV6nCJ5M",
+    authDomain: "project1930-24a81.firebaseapp.com",
+    databaseURL: "https://project1930-24a81.firebaseio.com",
+    projectId: "project1930-24a81",
+    storageBucket: "project1930-24a81.appspot.com",
+    messagingSenderId: "819128019702"
+  };
+  firebase.initializeApp(config);
+  app_firebase = firebase
+})();
