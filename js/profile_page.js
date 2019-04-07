772b926ecd874dc7f220aa296f15d2c8d99add0e
Jacky Zheng <30736593+jackywzheng@users.noreply.github.com>
2019-04-07 13:53:40 -0700

    Changed date of birth from text to dropdown menu

--- js/profile_page.js
+++ js/profile_page.js
@@ -9,7 +9,9 @@ var uid = null
                     user.getIdToken().then(function (accessToken) {
                         document.getElementById('name').value = displayName;
                         firebase.database().ref().child("users/" + uid + "/address").on("value", function (snap) { document.getElementById('address').value = snap.val() })
-                        firebase.database().ref().child("users/" + uid + "/date_of_birth").on("value", function (snap) { document.getElementById('date_of_birth').value = snap.val() })
+                        firebase.database().ref().child("users/" + uid + "/month").on("value", function (snap) { document.getElementById('month').value = snap.val() })
+                        firebase.database().ref().child("users/" + uid + "/day").on("value", function (snap) { document.getElementById('day').value = snap.val() })
+                        firebase.database().ref().child("users/" + uid + "/year").on("value", function (snap) { document.getElementById('year').value = snap.val() })
                         firebase.database().ref().child("users/" + uid + "/restrictions").on("value", function (snap) { document.getElementById('restrictions').value = snap.val() })
                         firebase.database().ref().child("users/" + uid + "/biography").on("value", function (snap) { document.getElementById('biography').value = snap.val() })
                     });
@@ -32,13 +34,17 @@ function saveform() {
     var form = document.getElementById("form");
     var name = form.elements[0].value;
     var address = form.elements[1].value;
-    var date_of_birth = form.elements[2].value;
-    var restrictions = form.elements[3].value;
-    var biography = form.elements[4].value;
+    var month = form.elements[2].value;
+    var day = form.elements[3].value;
+    var year = form.elements[4].value;
+    var restrictions = form.elements[5].value;
+    var biography = form.elements[6].value;
     firebase.database().ref("users/" + uid).set({
         name: displayName,
         address: address,
-        date_of_birth: date_of_birth,
+        month: month,
+        day: day,
+        year: year,
         restrictions: restrictions,
         biography: biography
     });
